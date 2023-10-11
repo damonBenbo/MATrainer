b310e68d4a8d8396419e090094941f6f604338ce
damonBenbo <damon.benbo@gmail.com>
2023-10-11 09:32:40 -0600

    Added function to filter and sort most recent for home page

--- frontend/ma-trainer/src/pages/Home.js
+++ frontend/ma-trainer/src/pages/Home.js
@@ -4,14 +4,21 @@ import './Home.css';
 import useDataFetch from '../hooks/useDataFetch';
 
 function Home() {
-  const { data: techniques} = useDataFetch('http://localhost:5000/api/techniques');
-  const { data: weapons} = useDataFetch('http://localhost:5000/api/weapons');
-  const { data: forms} = useDataFetch('http://localhost:5000/api/forms');
-  
+  const { data: techniques } = useDataFetch('http://localhost:5000/api/techniques');
+  const { data: weapons } = useDataFetch('http://localhost:5000/api/weapons');
+  const { data: forms } = useDataFetch('http://localhost:5000/api/forms');
+
   // Function to extract the YouTube video ID from the URL
   function getYouTubeVideoId(url) {
     const match = url && url.match(/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?feature=player_embedded&v=))([^&?]+)/);
     return (match && match[1]) || '';
+  }
+
+  // Function to filter and sort the data to only show 3 most recent for each category
+  function filterAndSortData(data, limit = 3) {
+    return data
+      .sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) // Sort by created_at in descending order
+      .slice(0, limit); // Take only the first 'limit' items
   }
 
   return (
@@ -19,14 +26,14 @@ function Home() {
       <h1>Welcome to Martial Arts Trainer!</h1>
       <p>Learn something new, or train your subject!</p>
 
-      <div className="popular-lists">
-        <h2>Popular Techniques</h2>
+      <div className="recent-lists">
+        <h2>Recently Added Techniques</h2>
         <ul>
-          {techniques.map((technique) => (
+          {filterAndSortData(techniques).map((technique) => (
             <li key={technique.id}>
               <strong>{technique.name}</strong>
               <p>{technique.description}</p>
-              
+
               {/* Embed the YouTube video if the URL is not null */}
               {technique.video_url && (
                 <div className="iframe">
@@ -35,7 +42,6 @@ function Home() {
                     width="280"
                     height="155"
                     src={`https://www.youtube.com/embed/${getYouTubeVideoId(technique.video_url)}`}
-                    frameBorder="0"
                     allowFullScreen
                   ></iframe>
                 </div>
@@ -44,10 +50,10 @@ function Home() {
           ))}
         </ul>
       </div>
-      <div className="popular-lists">
-        <h2>Popular Weapons</h2>
+      <div className="recent-lists">
+        <h2>Recently Added Weapons</h2>
         <ul>
-          {weapons.map((weapon) => (
+          {filterAndSortData(weapons).map((weapon) => (
             <li key={weapon.id}>
               <strong>{weapon.name}</strong>
               <p>{weapon.description}</p>
@@ -58,14 +64,14 @@ function Home() {
           ))}
         </ul>
       </div>
-      <div className="popular-lists">
-        <h2>Popular Forms</h2>
+      <div className="recent-lists">
+        <h2>Recently Added Forms</h2>
         <ul>
-          {forms.map((form) => (
+          {filterAndSortData(forms).map((form) => (
             <li key={form.id}>
               <strong>{form.name}</strong>
               <p>{form.description}</p>
-              
+
               {/* Embed the YouTube video if the URL is not null */}
               {form.video_url && (
                 <div className="iframe">
@@ -74,7 +80,6 @@ function Home() {
                     width="280"
                     height="155"
                     src={`https://www.youtube.com/embed/${getYouTubeVideoId(form.video_url)}`}
-                    frameBorder="0"
                     allowFullScreen
                   ></iframe>
                 </div>
