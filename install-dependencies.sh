306fd2b3815be9c11efb4a55b1e7698ecfc57c04
damonBenbo <damon.benbo@gmail.com>
2023-11-11 11:00:20 -0700

    Added script that uses shell to run install dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Navigate to the frontend directory and install dependencies
+cd frontend/ma-trainer
+npm install
+
+# Navigate to the backend directory and install dependencies
+cd ../../backend
+npm install
