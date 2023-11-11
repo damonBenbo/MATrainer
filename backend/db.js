a4b32a1b6ab8e795b722280c4de4a30632ef808f
damonBenbo <damon.benbo@gmail.com>
2023-11-11 11:11:20 -0700

    Added env variable for database URL

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client(DATABASE_URL);
+const client = new Client(process.env(DATABASE_URL));
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
