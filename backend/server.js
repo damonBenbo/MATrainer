fc7e12cf60e763e73fb9cb7714a6aa65838ef7c2
damonBenbo <damon.benbo@gmail.com>
2023-11-12 10:48:21 -0700

    updating cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,18 @@ const ensureAuth = require('./ensureAuth');
 const app = express();
 const port = 5000;
 
-app.use(cors({ origin: 'https://matrainer.onrender.com' })); // Adjust the origin as needed
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'http://matrainer.onrender.com', 'https://matrainer.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+}); // Adjust the origin as needed
 app.use(bodyParser.json());
 
 // db.connect();
